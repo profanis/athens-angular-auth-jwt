8b7021def836154195cfa4835e6281c5f03eaf62
profanis <fanis@projectbeagle.com>
2018-09-07 12:47:41 +0300

    port on runtime

--- server.js
+++ server.js
@@ -9,8 +9,11 @@ app.use(express.static(__dirname + '/dist/meetup-auth-jwt'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/meetup-auth-jwt/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/meetup-auth-jwt/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log("App is running on port " + port);
+});
